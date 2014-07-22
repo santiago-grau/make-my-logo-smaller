d7710d87cd050a25a7dd77c457b3a59c1b9a3a4c
Santi Grau <santi.grau.torredemer@gmail.comgit>
2014-07-22 15:37:47 +0200

    bubbles and some basic code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+var conversation = [
+	{
+		user: 'designer',
+		copy: 'Here\'s the proposal for your logo, hope you like it',
+		pos: 0 
+	},
+	{
+		user: 'client',
+		copy: 'Hmmmmmmmmmm... you forgot some weird lines under the logo...',
+		pos: 200 
+	}
+]
 var animations = [
 	{
 		id: 'logo1',
@@ -5,7 +17,28 @@ var animations = [
 		end: 1000
 	}
 ];
-var two = new Two({ type: Two.Types['opengl'] }).appendTo($('#logo')[0]);
+
+
+
+
+var resize = function(){
+	var rect = logo.getBoundingClientRect();
+	console.log(two)
+	logo.translation.set(two.width/2, two.height/2);
+}
+var two = new Two({ type: Two.Types['opengl'], height: $(window).height(), width: $(window).width()}).appendTo($('#logo')[0]);
+var logo = two.interpret(document.getElementById("logo1")).center();
+logo.fill = '#000';
+resize();
+two.update();
+
+var addBubble = function(bubble){
+	console.log(bubble)
+}
+_.each(conversation, addBubble);
+
+$('#scroller').height($(window).height() + 1000)
+
 
 $(window).bind('scroll', function(e){
 	console.log($(window).scrollTop())
